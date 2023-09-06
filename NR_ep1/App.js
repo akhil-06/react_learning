245901c14a1d2e2431a0009e0946b6a61491d9ee
akhil-06 <svam1008@gmail.com>
2023-09-06 14:13:14 +0530

    episode-01

---
+++ NR_ep1/App.js
@@ -0,0 +1,25 @@
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "I am h1 tag"),
+    React.createElement("h2", {}, "I am h2 tag")]
+  )
+);
+
+// const heading = React.createElement(
+//   "h1",
+//   {
+//     id: "heading1",
+//     xyz: "abc",
+//   },
+//   "Hello world rom React!!"
+// );
+
+// console.log(heading);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
