af21fc8ce94bb5ef21681da0660d199c8e2db68c
akhil-06 <svam1008@gmail.com>
2023-09-09 13:05:33 +0530

    episode 2 done

--- NR_ep1/App.js
+++ NR_ep1/App.js
@@ -1,10 +1,13 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement(
     "div",
     { id: "child" },
-    [React.createElement("h1", {}, "I am h1 tag"),
+    [React.createElement("h1", {}, "I am Akhil Sharma tag"),
     React.createElement("h2", {}, "I am h2 tag")]
   )
 );
