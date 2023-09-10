8f8721a10daa733ca38e8f30d949d5de0cb6d260
akhil-06 <svam1008@gmail.com>
2023-09-10 12:19:19 +0530

    component composition done

--- NR_ep1/App.js
+++ NR_ep1/App.js
@@ -1,22 +1,33 @@
-import React from "react";
-import ReactDOM  from "react-dom/client";
-
-
-const heading = React.createElement("h1", {id:"heading1"}, "This is me Akhil Sharma")
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(heading);
-
-
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
 // JSX(transpiled before t reaches Js) => parcel => babel is doing
 
-
 //JSX => Babel transpiles it to React.createElement => ReactElement - JS Object => HTMLElement(render)
-const JSXheading = <h1>Akhil form JSX</h1>
+// const JSXheading = <h1>Akhil form JSX</h1>
 //if more than one line tag enclose in ()
 const heading2 = (<h1>
   Hello my name is akhil sharma, I am from Haryana!!!!
 </h1>)
+// console.log(JSXheading);
 
-console.log(JSXheading);
-root.render(JSXheading);
+const HeadingComponent = () => {
+  return <h1>This is functional Component</h1>;
+};
+
+
+const TitleComponent = () => {
+  return (<h1>This is title component 😇</h1>)
+}
+const HeadingComponent2 = () => (
+  <div id="container">
+    <TitleComponent/>
+    <h1 className="heading1">This is functional Component</h1>
+  </div>
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading2);
+root.render(<HeadingComponent2/>)
+
+// React Component
