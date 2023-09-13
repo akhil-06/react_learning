1c1c16920b3968764712de89114c067982d2bb66
akhil-06 <svam1008@gmail.com>
2023-09-13 19:03:06 +0530

    useState react hooks done

--- NR_ep1/src/components/Body.js
+++ NR_ep1/src/components/Body.js
@@ -1,11 +1,21 @@
 import RestrauntCard from "./RestrauntCard";
 import resList from "../utils/mockData";
+import { useState } from "react";
 
 const Body = () => {
+  // Local State Variable - Super powerful variable
+  const [listOfRestaurants, setListOfRestraunt] = useState(resList);
     return (
       <div className="body">
         <div className="filter">
-          <button className="filter-btn">Top rated Restraunts</button>
+          <button className="filter-btn" onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestraunt(filteredList);
+          }}>
+            Top rated Restraunts
+            </button>
         </div>
         <div className="res-container">
         {resList.map((restaurant) => (
